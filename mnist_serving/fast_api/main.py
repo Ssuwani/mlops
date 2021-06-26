54d1a733e3f5d683b723a6307fd0c8bd0ac0493b
ssuwani <jsuwan961205@gmail.com>
2021-06-27 00:01:54 +0900

    [Add] Mnist serving with fastapi

---
+++ mnist_serving/fast_api/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, UploadFile, File
+from io import BytesIO
+from PIL import Image
+from tensorflow.keras.models import load_model
+import numpy as np
+
+model_path = 'models/mnist_cnn.ckpt'
+model = load_model(model_path)
+
+input_shape = model.layers[0].input_shape
+
+app = FastAPI()
+
+
+@app.get('/')
+def root_route():
+    return {"error": "use GET /prediction instead of root route"}
+
+
+@app.post('/prediction')
+async def prediction_route(file: UploadFile = File(...)):
+    contents = await file.read()
+    pil_image = Image.open(BytesIO(contents))
+
+    pil_image = pil_image.resize((input_shape[1], input_shape[2]))
+    pil_image = pil_image.convert('L')
+
+    numpy_array = np.array(pil_image)
+    numpy_array = np.expand_dims(numpy_array, axis=-1)
+    numpy_array = numpy_array / 255.0
+    if numpy_array.sum() > 200:
+        numpy_array = 1 - numpy_array
+    prediction_array = np.array([numpy_array])
+
+    predictions = model.predict(prediction_array)
+    prediction = np.argmax(predictions[0])
+    return {"result": int(prediction)}
---
+++ mnist_serving/fast_api/train.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = np.expand_dims(train_images, axis=-1)
+test_images = np.expand_dims(test_images, axis=-1)
+print(np.ndim(train_images))
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPool2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPool2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print("Model Loss : {:.4f} Model Acc : {:.4f}".format(test_loss, test_acc))
+
+# model.save('models/mnist_cnn.ckpt')
+# print(model.summary())
