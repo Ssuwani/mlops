6fc4779808d720bb2ce29b1a61690f47978a8557
ssuwani <jsuwan961205@gmail.com>
2021-06-27 12:09:29 +0900

    [Add] train.py *_serving.py

---
+++ mnist_serving/bentoml_serving.py
@@ -0,0 +1,43 @@
+from tensorflow.keras.models import load_model
+from bentoml.artifact import TensorflowSavedModelArtifact
+from bentoml.adapters import ImageInput
+from PIL import Image
+import bentoml
+import numpy as np
+
+model_path = 'models/mnist_cnn.ckpt'
+model = load_model(model_path)
+
+input_shape = model.layers[0].input_shape
+
+
+def data_preprocessing(pil_image):
+    pil_image = pil_image.resize((input_shape[1], input_shape[2]))
+    pil_image = pil_image.convert('L')
+
+    numpy_array = np.array(pil_image)
+    numpy_array = np.expand_dims(numpy_array, axis=-1)  # (28, 28) to (28, 28, 1)
+    numpy_array = numpy_array / 255.0
+    if numpy_array.sum() > 200:
+        numpy_array = 1 - numpy_array
+    prediction_array = np.array([numpy_array])  # (28, 28, 1) to (1, 28, 28, 1)
+    return prediction_array
+
+
+@bentoml.env(pip_dependencies=['tensorflow', 'numpy', 'pillow', 'imageio'])
+@bentoml.artifacts([TensorflowSavedModelArtifact('model')])
+class MnistTensorflow(bentoml.BentoService):
+    @bentoml.api(input=ImageInput())
+    def predict(self, image):
+        pil_image = Image.fromarray(image)
+        prediction_array = data_preprocessing(pil_image)
+
+        predictions = model.predict(prediction_array)
+        prediction = np.argmax(predictions[0])
+        return {"result": int(prediction)}
+
+
+bento_service = MnistTensorflow()
+bento_service.pack('model', model)
+
+saved_path = bento_service.save()
---
+++ mnist_serving/fastapi_serving.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, UploadFile, File
+from tensorflow.keras.models import load_model
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import uvicorn
+
+model_path = 'models/mnist_cnn.ckpt'
+model = load_model(model_path)
+
+input_shape = model.layers[0].input_shape
+
+app = FastAPI()
+
+
+def data_preprocessing(pil_image):
+    pil_image = pil_image.resize((input_shape[1], input_shape[2]))
+    pil_image = pil_image.convert('L')
+
+    numpy_array = np.array(pil_image)
+    numpy_array = np.expand_dims(numpy_array, axis=-1)  # (28, 28) to (28, 28, 1)
+    numpy_array = numpy_array / 255.0
+    if numpy_array.sum() > 200:
+        numpy_array = 1 - numpy_array
+    prediction_array = np.array([numpy_array])  # (28, 28, 1) to (1, 28, 28, 1)
+    return prediction_array
+
+
+@app.get('/')
+def root_route():
+    return {"error": "use POST /prediction instead of root route"}
+
+
+@app.post('/prediction')
+async def prediction_route(file: UploadFile = File(...)):
+    contents = await file.read()
+    pil_image = Image.open(BytesIO(contents))
+
+    prediction_array = data_preprocessing(pil_image)
+
+    predictions = model.predict(prediction_array)
+    prediction = np.argmax(predictions[0])
+    return {"result": int(prediction)}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
